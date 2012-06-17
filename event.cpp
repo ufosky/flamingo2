319bc74ba34df170f3a06a69cdb37516cd26bde1
Brad Zeis <bradzeis@gmail.com>
2012-06-16 23:50:49 -0500

    Escape key now exits engine.

--- event.cpp
+++ event.cpp
@@ -12,6 +12,8 @@ int Flamingo::handle_events() {
     while(SDL_PollEvent(&sdl_event)) {
         if (sdl_event.type == SDL_QUIT) {
             return -1;
+        } else if (sdl_event.type == SDL_KEYDOWN && sdl_event.key.keysym.sym==SDLK_ESCAPE) {
+            return -1;
         }
     }
 
