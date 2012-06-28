4fa4ed9c707900635fa875157bb137ab8185bd8b
Brad Zeis <bradzeis@gmail.com>
2012-06-28 12:20:29 -0500

    FlamingoApp now uses a custom Flamingo subclass and attempts to load textures. It won't find them because of the way .apps are packaged.

---
+++ test/FlamingoApp/FlamingoApp/FlamingoApp.cpp
@@ -0,0 +1,9 @@
+//
+//  FlamingoApp.cpp
+//  FlamingoApp
+//
+//  Created by Brad. Zeis on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ test/FlamingoApp/FlamingoApp/FlamingoApp.h
@@ -0,0 +1,14 @@
+//
+//  FlamingoApp.h
+//  FlamingoApp
+//
+//  Created by Brad. Zeis on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef FlamingoApp_FlamingoApp_h
+#define FlamingoApp_FlamingoApp_h
+
+
+
+#endif
